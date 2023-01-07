33313f98b9c8a13f858dce690650e14efdc71321
scuba2105 <scuba2150@gmail.com>
2023-01-08 00:24:49 +1100

    added data population to select inputs

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -10,23 +10,48 @@ async function getJSON() {
     const parsedData = await data.json();
     capitalCities = parsedData;
     console.log(capitalCities);
-}
+};
 
-// Get the JSON data for capital cities.
-async function populateContinentData() {
-    await getJSON();
-    const continentsArray = capitalCities.reduce((acc, curr) => {
-        const currentContinent = curr.ContinentName;
+// Filter the cities array of objects 
+function filterCitiesObject(property, value) {
+    return capitalCities.filter(city => {
+        return city[property] == value;
+    })
+};
+
+// Generate dropdown options
+function generateDropdownValues(array) {
+    return array.map(item => {
+        return`<option value="${item.toLowerCase()}">${item}</option>`
+    }).join('');
+};
+
+// Generate an array for the selected property from the cities array of objects 
+function generateArray(citiesObjectArray, property) {
+    return citiesObjectArray.reduce((acc, curr) => {
+        const currentContinent = curr[property];
         if (!acc.includes(currentContinent)) {
             acc.push(currentContinent);
             return acc;
         }
         return acc;
-    },[]);
+    },[]).sort();
+};
 
-    continentsDropdown.innerHTML = continentsArray.map(continent => {
-        return`<option value="${continent.toLowerCase()}">${continent}</option>`
-    }).join('');
+// Get the JSON data for capital cities.
+async function populateContinentData() {
+    await getJSON();
+    const continentsArray = generateArray(capitalCities, 'ContinentName');
+
+    continentsDropdown.innerHTML = generateDropdownValues(continentsArray);
+
+    // Select the initial continent and generate the cities dropdown.
+    const initialContinent = continentsArray[0];
+    console.log(initialContinent);
+    const filteredCities = filterCitiesObject('ContinentName', initialContinent);
+    const citiesArray = generateArray(filteredCities, 'CapitalName');
+    
+    citiesDropdown.innerHTML = generateDropdownValues(citiesArray);
 };
 
 populateContinentData();
@@ -36,13 +61,41 @@ const travelButtons = document.querySelectorAll('.travel-button');
 const travelForm = document.querySelector('.travel-form');
 const continentsDropdown = document.querySelector('.continent-dropdown');
 const citiesDropdown = document.querySelector('.city-dropdown');
+const cancel = document.querySelector('.cancel');
+const submit = document.querySelector('.submit');
 
 // Add event listeners to each travel button.
 travelButtons.forEach(travelButton => {
     travelButton.addEventListener('click', showTravelForm);
-})
+});
+
+// Event listener for continents dropdown
+continentsDropdown.addEventListener('change', updateCities);
+
+// Cancel button event listener
+cancel.addEventListener('click', hideTravelForm);
+
 
 // Define the event listener functions
 function showTravelForm() {
-    console.log(this.classList);
-}
+    travelForm.style.visibility = 'visible';
+};
+
+function hideTravelForm() {
+    travelForm.style.visibility = 'hidden';
+};
+
+function updateCities() {
+    const newValue = this.value;
+    const newContinentWords = newValue.split(' ');
+    const newContinent = newContinentWords.map(word => {
+        return word[0].toUpperCase() + word.substring(1);
+    }).join(' ');
+    const filteredCities = filterCitiesObject('ContinentName', newContinent);
+    const citiesArray = generateArray(filteredCities, 'CapitalName');    
+    citiesDropdown.innerHTML = generateDropdownValues(citiesArray);
+};
+      
+
+
+
