ccdf84e7803b83d969b7ecedc9816605372d7960
scuba2105 <scuba2150@gmail.com>
2023-01-07 22:15:48 +1100

    Added travel form

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,7 +1,10 @@
+// initialise variable to store capital cities data.
 let capitalCities; 
 
+// Get root directory of web app.
 const directory = window.location.href;
 
+// Import the json data which is stored on the server.
 async function getJSON() {
     const data = await fetch(`${directory}capitals`);
     const parsedData = await data.json();
@@ -9,4 +12,37 @@ async function getJSON() {
     console.log(capitalCities);
 }
 
-getJSON();
+// Get the JSON data for capital cities.
+async function populateContinentData() {
+    await getJSON();
+    const continentsArray = capitalCities.reduce((acc, curr) => {
+        const currentContinent = curr.ContinentName;
+        if (!acc.includes(currentContinent)) {
+            acc.push(currentContinent);
+            return acc;
+        }
+        return acc;
+    },[]);
+
+    continentsDropdown.innerHTML = continentsArray.map(continent => {
+        return`<option value="${continent.toLowerCase()}">${continent}</option>`
+    }).join('');
+};
+
+populateContinentData();
+
+// Define the elements on the page.
+const travelButtons = document.querySelectorAll('.travel-button');
+const travelForm = document.querySelector('.travel-form');
+const continentsDropdown = document.querySelector('.continent-dropdown');
+const citiesDropdown = document.querySelector('.city-dropdown');
+
+// Add event listeners to each travel button.
+travelButtons.forEach(travelButton => {
+    travelButton.addEventListener('click', showTravelForm);
+})
+
+// Define the event listener functions
+function showTravelForm() {
+    console.log(this.classList);
+}
