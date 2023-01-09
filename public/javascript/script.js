d67e912e6f14d9a762f772cc7cc5fb742a5c9c2f
scuba2105 <scuba2150@gmail.com>
2023-01-09 23:25:30 +1100

    updated front end

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,18 @@
+// Define the elements on the page.
+const travelButtons = document.querySelectorAll('.travel-button');
+const travelForm = document.querySelector('.travel-form');
+const continentsDropdown = document.querySelector('.continent-dropdown');
+const countriesDropdown = document.querySelector('.country-dropdown');
+const cityLabel = document.querySelector('.city-label')
+const cancel = document.querySelector('.cancel');
+const submit = document.querySelector('.submit');
+const subject = document.querySelector('.subject');
+const currentLocations = document.querySelectorAll('.current-location');
+const currentContinents = document.querySelectorAll('.continent-location');
+const currentCountries = document.querySelectorAll('.country-location')
+const counts = document.querySelectorAll('.count');
+const mapContainer = document.querySelector('.world-map');
+
 // Set the web socket
 const travelSocketUrl = 'ws://localhost:5050/'
 const socket = new WebSocket(travelSocketUrl);
@@ -16,13 +31,14 @@ socket.onmessage = e => {
     // Extract the data from the returned data object
     const index = returnedObject.index;
     const currentLocation = returnedObject["New Location"];
-    const previousLocation = returnedObject["Old Location"];
+    const currentContinent = returnedObject["New Continent"];
+    const currentCountry = returnedObject["New Country"];
     const newCount = returnedObject["Travel Count"];
     
     // Set the new values in the info display
-    currentLocations[index].textContent = `Current Location: ${currentLocation}`;
-    previousLocations[index].textContent = `Previous Location: ${previousLocation}`;
-    counts[index].textContent = `No. of Times Travelled: ${newCount}`;
+    currentLocations[index].innerHTML = `<strong>Current Location:</strong> ${currentLocation}`;
+    currentContinents[index].innerHTML = `<strong>Current Continent:</strong> ${currentContinent}`;
+    counts[index].innerHTML = `<strong>No. of Times Travelled:</strong> ${newCount}`;
 }
 
 // initialise variable to store capital cities data.
@@ -37,6 +53,17 @@ async function getJSON() {
     const parsedData = await data.json();
     capitalCities = parsedData;
     console.log(capitalCities);
+};
+
+// Capitalise first letters of a word
+function capitaliseFirstLetters(word) {
+    const stringArray = word.split(' ');
+    return stringArray.map(word => {
+        if (word == 'and') {
+            return word;
+        }
+        return word[0].toUpperCase() + word.substring(1);
+    }).join(' ');
 };
 
 // Filter the cities array of objects 
@@ -68,32 +95,25 @@ function generateArray(citiesObjectArray, property) {
 // Get the JSON data for capital cities.
 async function populateContinentData() {
     await getJSON();
+    
+    // Populate the continents in the dropdown values.
     const continentsArray = generateArray(capitalCities, 'ContinentName');
-
     continentsDropdown.innerHTML = generateDropdownValues(continentsArray);
 
     // Select the initial continent and generate the cities dropdown.
     const initialContinent = continentsArray[0];
     const filteredCities = filterCitiesObject('ContinentName', initialContinent);
-    const citiesArray = generateArray(filteredCities, 'CapitalName');
-    
-    citiesDropdown.innerHTML = generateDropdownValues(citiesArray);
+    const countriesArray = generateArray(filteredCities, 'CountryName');
+    countriesDropdown.innerHTML = generateDropdownValues(countriesArray);
+
+    // Select the initial coountry and generate the capital city.
+    const initialCountry = countriesArray[0];
+    const filteredCountries = filterCitiesObject('CountryName', initialCountry)[0];
+    const capitalCity = filteredCountries.CapitalName;
+    cityLabel.innerHTML = capitalCity
 };
 
 populateContinentData();
-
-// Define the elements on the page.
-const travelButtons = document.querySelectorAll('.travel-button');
-const travelForm = document.querySelector('.travel-form');
-const continentsDropdown = document.querySelector('.continent-dropdown');
-const citiesDropdown = document.querySelector('.city-dropdown');
-const cancel = document.querySelector('.cancel');
-const submit = document.querySelector('.submit');
-const subject = document.querySelector('.subject');
-const currentLocations = document.querySelectorAll('.current-location');
-const previousLocations = document.querySelectorAll('.previous-location');
-const counts = document.querySelectorAll('.count');
-const mapContainer = document.querySelector('.world-map');
 
 // Add event listeners to each travel button.
 travelButtons.forEach(travelButton => {
@@ -107,9 +127,10 @@ cancel.addEventListener('click', hideTravelForm);
 submit.addEventListener('click', submitTravelForm);
 
 // Event listener for continents dropdown
-continentsDropdown.addEventListener('change', updateCities);
+continentsDropdown.addEventListener('change', updateCountries);
 
-
+// Event listener for continents dropdown
+countriesDropdown.addEventListener('change', updateCities);
 
 // Define the event listener functions
 function showTravelForm() {
@@ -133,26 +154,22 @@ function submitTravelForm() {
     // Index (0-2) determines what players info panel is to be changed
     const index = Number(id.split(' ')[1]) - 1;
     
-    // Calculate the old location based on current location before travel
-    const newLocationText = currentLocations[index].textContent;
-    const oldLocation = newLocationText.split(': ')[1];
+    // Calculate the new continent location
+    const newContinent = capitaliseFirstLetters(continentsDropdown.value); 
 
     // Calculate the new location value
-    const locationValue = citiesDropdown.value;
+    const newCountry = capitaliseFirstLetters(countriesDropdown.value);
     
-    // Capitalise first letters
-    const stringArray = locationValue.split(' ');
-    const newLocation = stringArray.map(word => {
-        return word[0].toUpperCase() + word.substring(1);
-    }).join(' ');
-        
+    // Calculate the new capital city
+    const capitalCity = capitaliseFirstLetters(cityLabel.textContent);
+
     // Calculate the count 
     const oldCountText = counts[index].textContent;
     const newCountText = oldCountText.split(': ')[1];
     const newCount = Number(newCountText) + 1;
 
     // Calculate the count
-    const dataObject = {'index': index, 'New Location': newLocation, 'Old Location': oldLocation, 'Travel Count': newCount}; 
+    const dataObject = {'index': index, 'New Location': capitalCity, 'New Continent': newContinent, 'New Country': newCountry, 'Travel Count': newCount}; 
     socket.send(JSON.stringify(dataObject));
 
     // Close the form
@@ -160,23 +177,34 @@ function submitTravelForm() {
     travelForm.style.visibility = 'hidden';
 }
 
+function updateCountries() {
+    cityLabel.textContent = ''
+    const newValue = this.value;
+    const newContinent = capitaliseFirstLetters(newValue);
+    const filteredCountries = filterCitiesObject('ContinentName', newContinent);
+    const countriesArray = generateArray(filteredCountries, 'CountryName');    
+    countriesDropdown.innerHTML = generateDropdownValues(countriesArray);
+
+    // Initialise the city
+    const initialCountry = countriesArray[0];
+    const newCountryObject = filterCitiesObject('CountryName', initialCountry)[0];
+    const initialCapital = newCountryObject.CapitalName;
+    cityLabel.textContent = initialCapital;
+};
+  
 function updateCities() {
     const newValue = this.value;
-    const newContinentWords = newValue.split(' ');
-    const newContinent = newContinentWords.map(word => {
-        return word[0].toUpperCase() + word.substring(1);
-    }).join(' ');
-    const filteredCities = filterCitiesObject('ContinentName', newContinent);
-    const citiesArray = generateArray(filteredCities, 'CapitalName');    
-    citiesDropdown.innerHTML = generateDropdownValues(citiesArray);
+    const newCountry = capitaliseFirstLetters(newValue);
+    const filteredCountry = filterCitiesObject('CountryName', newCountry)[0];
+    const capitalCity = filteredCountry.CapitalName;    
+    cityLabel.innerHTML = capitalCity;
 };
-      
 
 // Access SVG elements
 const map = document.querySelector(".map-image");
 
 // It's important to add an load event listener to the object,
-// as it will load the svg doc asynchronously
+// as it will load the svg doc asynchronously.
 map.addEventListener("load",function(){
 
     // get the inner DOM of alpha.svg
@@ -192,7 +220,7 @@ map.addEventListener("load",function(){
     russia.addEventListener("click",function(){
         clicked = !clicked;
         if (clicked == true) {
-            this.style.fill = 'rgb(228, 186, 128)';
+            this.style.fill = 'red';
             const boundingRect = this.getBoundingClientRect();
             const x =  boundingRect.x;
             const y = boundingRect.y;
@@ -208,7 +236,7 @@ map.addEventListener("load",function(){
             mapContainer.append(dot);  
         }
         else {
-            this.style.fill = 'red';
+            this.style.fill = 'rgb(228, 186, 128)';
         }
     }, false);
 }, false);
